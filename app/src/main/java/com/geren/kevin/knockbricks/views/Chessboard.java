7571d7ab85008b3549e90b1f5b490ff3d263aaf2
kevin <kevin4oct@163.com>
2018-12-03 18:29:00 +0800

    新建项目

--- app/src/main/java/com/geren/kevin/knockbricks/views/Chessboard.java
+++ app/src/main/java/com/geren/kevin/knockbricks/views/Chessboard.java
@@ -4,15 +4,20 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.geren.kevin.knockbricks.main.bean.Block;
+import com.geren.kevin.knockbricks.utils.TransformUtils;
 
 public class Chessboard extends View {
 
     public static final String TAG = Chessboard.class.getSimpleName();
+
+    private ClickListener clickListener;
 
     //画笔
     private Paint paint;
@@ -22,6 +27,7 @@ public class Chessboard extends View {
     private int size = 1;
     private int row = 1;
     private int paintWith;
+    private Context context;
 
     public Chessboard(Context context) {
         this(context, null);
@@ -33,11 +39,18 @@ public class Chessboard extends View {
 
     public Chessboard(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
         data = new Block[100][100];
         paint = new Paint();
-        paintWith = 10;
+        paint.setAntiAlias(true);
+        paintWith = 1;
         paint.setStrokeWidth(paintWith);
     }
+
+    public void setOnBoardClickListener(ClickListener listener) {
+        this.clickListener = listener;
+    }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -55,8 +68,7 @@ public class Chessboard extends View {
 
         int width = getWidth();
         int height = getHeight();
-        size = width > height ? height : width;
-
+        this.size = width > height ? height : width;
         this.row = row;
         this.data = data;
         //重绘
@@ -67,24 +79,65 @@ public class Chessboard extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         //
-//        row = 10;
         canvas.drawColor(Color.GREEN);
         //
-        int boxWidth = (size - (paintWith * (size + 1))) / (row);
-
-        Log.e(TAG, "size=" + size + ",row=" + row + ",boxWidth =" + boxWidth);
+        int paintWith2dip = TransformUtils.px2dip(context, paintWith);
+        int boxWidth = (size - (paintWith2dip * (row))) / (row);
 
         //画网格
         paint.setColor(Color.GRAY);
-        for (int i = 0; i < row; i++) {//画横线
-            canvas.drawLine(0, -boxWidth * i * 2, size, -boxWidth * i, paint);
+        for (int i = 0; i <= row; i++) {//画横线
+            canvas.drawLine(paintWith2dip * 2, boxWidth * i, size, boxWidth * i, paint);
         }
-        for (int i = 0; i < row; i++) {//画竖线
-            canvas.drawLine(-boxWidth * i * 2, 0, -boxWidth * i, size, paint);
+        for (int i = 0; i <= row; i++) {//画竖线
+            canvas.drawLine(boxWidth * i, paintWith2dip * 2, boxWidth * i, size, paint);
         }
 
+        //画棋子图片
+        for (int i = 0; i < data.length; i++) {
+            Block[] datum = data[i];
+            for (int j = 0; j < datum.length; j++) {
+                Block block = datum[j];
+                int value = 0;
+                try {
+                    value = block.getValue();
+                } catch (Exception e) {
+                    value = 1;
+                }
+                switch (value) {
+                    case 1:
+                        paint.setColor(Color.BLUE);
+                        break;
+                    case 2:
+                        paint.setColor(Color.YELLOW);
+                        break;
+                    case 3:
+                        paint.setColor(Color.RED);
+                        break;
+                }
+                int x = boxWidth * i + boxWidth / 2;
+                int y = boxWidth * j + boxWidth / 2;
+                paint.setTextSize(50);
+                paint.setTextAlign(Paint.Align.CENTER);
+                paint.setTypeface(Typeface.DEFAULT_BOLD);
+                canvas.drawText("" + value, x, y, paint);
 
+            }
+        }
     }
 
+    public interface ClickListener {
+        void chessClicked(Block block);
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float rawX = event.getRawX();
+        float rawY = event.getRawY();
+        Log.e(TAG, "rawX: " + rawX + ",rawY:" + rawY);
+
+//        rawX/
+
+        return true;
+    }
 }
